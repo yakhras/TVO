6fd51c38f1837aa962f46b6ce72f061d4d357684
yakhras <it@menagate.com.tr>
2026-09-25 09:35:40 +0300

    Add 2 reports

--- logistics/__manifest__.py
+++ logistics/__manifest__.py
@@ -37,6 +37,7 @@
         'views/logistics_container_child_state_views.xml',
         'views/actions_menus.xml',
         'views/logistics_requisition_mismatch_report_views.xml',
+        'views/logistics_container_line_report_views.xml',
         'views/sale_container_line_views.xml',
     ],
     'assets': {
